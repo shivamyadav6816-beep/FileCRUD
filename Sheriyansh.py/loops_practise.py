0799f035f383ccdbc98e6452e4346e7f360d2993
shivam singh yadav <shivamyadav6816@gmial.com>
2026-05-14 18:14:22 +0530

    commit

---
+++ Sheriyansh.py/loops_practise.py
@@ -0,0 +1,21 @@
+# n = int(input("enter the number"))
+# for i in range(1,n+1 ,1):
+#     print(i)
+
+
+# n = int(input("enter the number"))
+# for i in range(n,11):
+#    print(n*i)
+
+# n = int(input("enter the number"))
+# for i in range(1,11):
+#     print(f"{n} x {i} = {n*i}")
+
+
+
+# n = int (input ("enter the number"))
+
+def hello():
+    n = input("enetr the word")
+    
+    
